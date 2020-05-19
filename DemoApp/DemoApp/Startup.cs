06f544478ad92f7e01c7fc9a44f8c40b6a3157a0
Suraj Bose <Suraj.Bose@au.ey.com>
2020-05-19 14:15:02 +1000

    added Data project and created migrations

--- DemoApp/DemoApp/Startup.cs
+++ DemoApp/DemoApp/Startup.cs
@@ -8,10 +8,10 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using DemoApp.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using DempApp.Data;
 
 namespace DemoApp
 {
@@ -27,11 +27,11 @@ namespace DemoApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<ApplicationDbConnection>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<ApplicationDbConnection>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
@@ -58,6 +58,7 @@ namespace DemoApp
             app.UseAuthentication();
             app.UseAuthorization();
 
+            //app.UseMvc();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
