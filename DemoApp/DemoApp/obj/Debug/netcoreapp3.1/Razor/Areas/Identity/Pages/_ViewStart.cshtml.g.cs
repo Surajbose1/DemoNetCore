018c9f0831addbb65672eb932da44ef8b065c0dd
Suraj Bose <Suraj.Bose@au.ey.com>
2020-05-19 14:29:00 +1000

    scaffolded identity pages

--- DemoApp/DemoApp/obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
+++ DemoApp/DemoApp/obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
@@ -12,7 +12,23 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#line 1 "C:\Users\bosesu\Source\Repos\DemoNetCore\DemoApp\DemoApp\Areas\Identity\Pages\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Identity;
+
+#line default
+#line hidden
+#line 2 "C:\Users\bosesu\Source\Repos\DemoNetCore\DemoApp\DemoApp\Areas\Identity\Pages\_ViewImports.cshtml"
+using DemoApp.Areas.Identity;
+
+#line default
+#line hidden
+#line 3 "C:\Users\bosesu\Source\Repos\DemoNetCore\DemoApp\DemoApp\Areas\Identity\Pages\_ViewImports.cshtml"
+using DemoApp.Areas.Identity.Pages;
+
+#line default
+#line hidden
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"13b82ef97968a56a4de533f67cf3e21d662f41aa", @"/Areas/Identity/Pages/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"f83728ea865bca824b3ae883f3bb1df920e3c070", @"/Areas/Identity/Pages/_ViewImports.cshtml")]
     public class Areas_Identity_Pages__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
