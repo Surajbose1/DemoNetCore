3e9be31a1755d335263af276fdde9f7c0c13e854
suraj bose <suraj.bose@au.ey.com>
2020-06-02 16:04:42 +1000

    save changes to execute stored proc

---
+++ DemoApp/DemoApp/Controllers/StoredProcController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DempApp.Data;
+using DempApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class StoredProcController : Controller
+    {
+        private ApplicationDbConnection _context;
+
+        public StoredProcController(ApplicationDbConnection context)
+        {
+            _context = context;
+        }
+
+
+        //fetch data using Stored Proc, no parameters
+        public IActionResult Index()
+        {
+            var data = new SP_Call(_context).ReturnList<Vehicle>("GetAllVehicles");
+            return View("~/Views/Crud/Index.cshtml", data);
+        }
+    }
+}
---
+++ DemoApp/DempApp.Data/Models/SP_Call.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DempApp.Data.Models
+{
+   public class SP_Call
+    {
+        private readonly ApplicationDbConnection _db;
+        private static string ConnectionString = "";
+
+        public SP_Call(ApplicationDbConnection db)
+        {
+            _db = db;
+            ConnectionString = db.Database.GetDbConnection().ConnectionString;
+        }
+
+        public IEnumerable<T> ReturnList<T>(string procedureName)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+    }
+}
