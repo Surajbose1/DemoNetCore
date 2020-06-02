e27a3892cb574a128c7dbba54bd2ffce8492aae6
suraj bose <suraj.bose@au.ey.com>
2020-06-02 16:52:00 +1000

    added stored proc

--- DemoApp/DemoApp/Controllers/StoredProcController.cs
+++ DemoApp/DemoApp/Controllers/StoredProcController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Dapper;
+﻿using Dapper;
 using DempApp.Data;
 using DempApp.Data.Models;
 using Microsoft.AspNetCore.Mvc;
