418ed79b4939e7a37ac5a3ac7af8efa599efa826
suraj bose <suraj.bose@au.ey.com>
2020-06-02 16:20:00 +1000

    execute stored proc with parameters

--- DemoApp/DemoApp/Controllers/StoredProcController.cs
+++ DemoApp/DemoApp/Controllers/StoredProcController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using DempApp.Data;
 using DempApp.Data.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +23,16 @@ namespace DemoApp.Controllers
         public IActionResult Index()
         {
             var data = new SP_Call(_context).ReturnList<Vehicle>("GetAllVehicles");
+            return View("~/Views/Crud/Index.cshtml", data);
+        }
+
+        //fetch data using Stored Proc, filter by input parameters
+        [Route("{Controller}/{Action}/{make:alpha}")]
+        public IActionResult GetVehicleByMake(string make)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@vehicleMake", make);
+            var data = new SP_Call(_context).ReturnList<Vehicle>("GetVehiclesByMake", param);
             return View("~/Views/Crud/Index.cshtml", data);
         }
     }
--- DemoApp/DempApp.Data/Models/SP_Call.cs
+++ DemoApp/DempApp.Data/Models/SP_Call.cs
@@ -26,5 +26,33 @@ namespace DempApp.Data.Models
                 return sqlCon.Query<T>(procedureName, commandType: System.Data.CommandType.StoredProcedure);
             }
         }
+
+        public IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public T ExecuteReturnScaler<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.ExecuteScalar<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure), typeof(T));
+            }
+        }
+
+        public void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+         
     }
 }
