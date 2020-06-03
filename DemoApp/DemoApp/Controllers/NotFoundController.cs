54267dbb745845d3f122b08c580e5d194211c9bb
suraj bose <suraj.bose@au.ey.com>
2020-06-03 17:05:47 +1000

    implement the NotFound Controller

---
+++ DemoApp/DemoApp/Controllers/NotFoundController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    /// <summary>
+    /// In order to implement the NotFound controller locally, do the following
+    /// 1. Change the environment to production in launchsettings.json
+    /// 2.Change the startup.cs file to use UseStatusCodePagesWithReExecute
+    /// 3.Implement the NotfoundController
+    /// </summary>
+    public class NotFoundController : Controller
+    {
+        [Route("NotFound/{statusCode}")]
+        public IActionResult NotFoundAction(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Resource not found";
+                    ViewBag.Path = statusCodeResult.OriginalPath;
+                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    break;
+            }
+            return View();
+        }
+    }
+}
--- DemoApp/DemoApp/Startup.cs
+++ DemoApp/DemoApp/Startup.cs
@@ -65,7 +65,8 @@ namespace DemoApp
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("/NotFound/{0}");
+                 // app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
