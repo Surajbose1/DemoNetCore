c06ba3c68fed64874e9c2bd595a841eb211d0676
suraj bose <suraj.bose@au.ey.com>
2020-06-03 14:33:58 +1000

    implementing logging using Nlog and Default logging provider

---
+++ DemoApp/DemoApp/Controllers/LoggingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DemoApp.Controllers
+{
+    public class LoggingController : Controller
+    {
+        //Default logging provider
+        private ILogger _logger;
+
+        public LoggingController(ILogger<LoggingController> logger)
+        {
+            _logger = logger;
+        }
+         
+        //steps to configure Nlog
+        //1. Install Nlog.Web.AspNetCore nuget package
+        //2.Create nlog.config file, change copy settings by looking up the properties
+        //3.Add Nlog as one of the Logging provider in Program.cs
+        public IActionResult Index()
+        {
+            throw new Exception("Testing Nlog Logging");
+        }
+
+
+        public IActionResult TestLog()
+        {
+            _logger.LogInformation("Test1");
+            _logger.LogDebug("Test2");
+            _logger.LogError("Test3");
+            _logger.LogCritical("Test4");
+            _logger.LogWarning("Test5");
+
+            return Content("Logging Done");
+        }
+
+    }
+}
--- DemoApp/DemoApp/Program.cs
+++ DemoApp/DemoApp/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace DemoApp
 {
@@ -18,6 +15,15 @@ namespace DemoApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                    //configure Nlog by copying this entire method from aspnetcore/src/DefaultBuilder/src/WebHost.cs(github repo)
+                    logging.AddNLog();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
